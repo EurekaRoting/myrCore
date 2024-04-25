dc2426f65539744979b850f4c0fa1cae4a1f3ae5
EurekaRoting <roting68@gmail.com>
2024-04-25 15:05:57 +0800

    new ch2

---
+++ os/target/debug/build/os-bb0e74f81fa807fa/build_script_build-bb0e74f81fa807fa.d
@@ -0,0 +1,5 @@
+/home/oslab/rCore-T/os/target/debug/build/os-bb0e74f81fa807fa/build_script_build-bb0e74f81fa807fa: build.rs
+
+/home/oslab/rCore-T/os/target/debug/build/os-bb0e74f81fa807fa/build_script_build-bb0e74f81fa807fa.d: build.rs
+
+build.rs:
